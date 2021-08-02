2269895750d8ff437628b379c036b9134ab5d082
Lekesoldat <magnulho@gmail.com>
2021-08-02 20:46:31 +0200

    There will always be someone better than you. Give up.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use std::fs::File;
-use rocket::{State, serde::{Serialize, Deserialize, json::Json}};
 use rand::{thread_rng, Rng};
+use rocket::{
+    fs::FileServer,
+    serde::{json::Json, Deserialize, Serialize},
+    State,
+};
+use std::fs::File;
 
 #[derive(Serialize, Deserialize)]
 struct Quote {
@@ -21,5 +26,8 @@ fn quote(quotes: &State<Vec<Quote>>) -> Json<&Quote> {
 fn rocket() -> _ {
     let file = File::open("unspirational.json").unwrap();
     let quotes: Vec<Quote> = serde_json::from_reader(file).unwrap();
-    rocket::build().manage(quotes).mount("/", routes![quote])
+    rocket::build()
+        .manage(quotes)
+        .mount("/", FileServer::from("static"))
+        .mount("/", routes![quote])
 }
